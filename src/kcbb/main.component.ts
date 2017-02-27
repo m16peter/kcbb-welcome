46011454a904bc1814a4bb8c9f0da50320118044
Martin-Peter Lakatos <m16peter@yahoo.ro>
2017-02-27 01:25:14 +0100

    ...

--- src/kcbb/main.component.ts
+++ src/kcbb/main.component.ts
@@ -17,11 +17,11 @@ export class MainComponent {
             "content": {}
         };
         // url-paths to slides...
-        this.main['slider'].slides = [
-            '/kcbb-welcome/assets/img/1.jpg',
-            '/kcbb-welcome/assets/img/2.jpg',
-            '/kcbb-welcome/assets/img/3.jpg'
-        ];
+        this.main['slider'].slides = [];
+        for (let i = 1; i < 4; i++) {
+            let url: string = '/kcbb-welcome/assets/img/' + i + '.jpg';
+            this.main['slider'].slides.push(url);
+        }
         // link names
         this.main['nav-bar'] = {
             'active-link': 0
@@ -30,28 +30,15 @@ export class MainComponent {
         this.main['content'] = [
             {
                 'title': 'Lorem Ipsum',
-                'description': ''
-            },
-            {
-                'title': 'Čo je to Lorem Ipsum?',
                 'description': '<p>Lorem Ipsum je fiktívny text, používaný pri návrhu tlačovín a typografie. Lorem Ipsum je štandardným výplňovým textom už od 16. storočia, keď neznámy tlačiar zobral sadzobnicu plnú tlačových znakov a pomiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. Spopularizovaný bol v 60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali pasáže Lorem Ipsum, a neskôr aj publikačným softvérom ako Aldus PageMaker, ktorý obsahoval verzie Lorem Ipsum.</p>'
             },
             {
-                'title': 'Prečo ho používame?',
-                'description': '<p>Je dávno známe, že ak je zrozumiteľný obsah stránky, na ktorej rozloženie sa čitateľ díva, jeho pozornosť je rozptýlená.</p>' +
-                               '<p>Dôvodom použitia Lorem Ipsum je fakt, že má viacmenej normálne rozloženie písmen, takže oproti použitiu "Sem príde text, sem príde text" sa obsah vypĺňanej oblasti na stránke viac podobá na skutočný text. Mnohé balíky publikačného softvéru a editorov webových stránok už používajú Lorem Ipsum ako predvolený výplňový text a keď dáte na internete vyhľadávať "lorem ipsum", objavíte mnoho webových stránok v rannom štádiu ich vzniku. V minulých rokoch sa objavilo mnoho verzií tohto textu, niekedy náhodou, niekedy úmyselne (pridaný humor a podobne).</p>'
-            },
-            {
-                'title': 'Odkiaľ pochádza?',
+                'title': 'Odkiaľ pochádza Lorem Ipsum?',
                 'description': '<p>Napriek všeobecnému presvedčeniu nie je Lorem Ipsum len náhodný text. Jeho korene sú v časti klasickej latinskej literatúry z roku 45 pred n.l., takže má viac ako 2000 rokov. Richard McClintock, profesor latinčiny na Hampden-Sydney College vo Virgínii, hľadal jedno z menej určitých latinských slov, consectetur, z pasáže Lorem Ipsum, a ako vyhľadával výskyt tohto slova v klasickej literatúre, objavil jeho nepochybný zdroj. Lorem Ipsum pochádza z odsekov 1.10.32 a 1.10.33 Cicerovho diela "De finibus bonorum et malorum" (O najvyššom dobre a zle), napísaného v roku 45 pred n.l. Táto kniha je pojednaním o teórii etiky, a bola veľmi populárna v renesancii. Prvý riadok Lorem Ipsum, "Lorem ipsum dolor sit amet..", je z riadku v odseku 1.10.32.</p>' +
                                '<p>Napriek všeobecnému presvedčeniu nie je Lorem Ipsum len náhodný text. Jeho korene sú v časti klasickej latinskej literatúry z roku 45 pred n.l., takže má viac ako 2000 rokov. Richard McClintock, profesor latinčiny na Hampden-Sydney College vo Virgínii, hľadal jedno z menej určitých latinských slov, consectetur, z pasáže Lorem Ipsum, a ako vyhľadával výskyt tohto slova v klasickej literatúre, objavil jeho nepochybný zdroj. Lorem Ipsum pochádza z odsekov 1.10.32 a 1.10.33 Cicerovho diela "De finibus bonorum et malorum" (O najvyššom dobre a zle), napísaného v roku 45 pred n.l. Táto kniha je pojednaním o teórii etiky, a bola veľmi populárna v renesancii. Prvý riadok Lorem Ipsum, "Lorem ipsum dolor sit amet..", je z riadku v odseku 1.10.32.</p>' +
                                '<p>Napriek všeobecnému presvedčeniu nie je Lorem Ipsum len náhodný text. Jeho korene sú v časti klasickej latinskej literatúry z roku 45 pred n.l., takže má viac ako 2000 rokov. Richard McClintock, profesor latinčiny na Hampden-Sydney College vo Virgínii, hľadal jedno z menej určitých latinských slov, consectetur, z pasáže Lorem Ipsum, a ako vyhľadával výskyt tohto slova v klasickej literatúre, objavil jeho nepochybný zdroj. Lorem Ipsum pochádza z odsekov 1.10.32 a 1.10.33 Cicerovho diela "De finibus bonorum et malorum" (O najvyššom dobre a zle), napísaného v roku 45 pred n.l. Táto kniha je pojednaním o teórii etiky, a bola veľmi populárna v renesancii. Prvý riadok Lorem Ipsum, "Lorem ipsum dolor sit amet..", je z riadku v odseku 1.10.32.</p>' +
                                '<p>Napriek všeobecnému presvedčeniu nie je Lorem Ipsum len náhodný text. Jeho korene sú v časti klasickej latinskej literatúry z roku 45 pred n.l., takže má viac ako 2000 rokov. Richard McClintock, profesor latinčiny na Hampden-Sydney College vo Virgínii, hľadal jedno z menej určitých latinských slov, consectetur, z pasáže Lorem Ipsum, a ako vyhľadával výskyt tohto slova v klasickej literatúre, objavil jeho nepochybný zdroj. Lorem Ipsum pochádza z odsekov 1.10.32 a 1.10.33 Cicerovho diela "De finibus bonorum et malorum" (O najvyššom dobre a zle), napísaného v roku 45 pred n.l. Táto kniha je pojednaním o teórii etiky, a bola veľmi populárna v renesancii. Prvý riadok Lorem Ipsum, "Lorem ipsum dolor sit amet..", je z riadku v odseku 1.10.32.</p>' +
                                '<p>Napriek všeobecnému presvedčeniu nie je Lorem Ipsum len náhodný text. Jeho korene sú v časti klasickej latinskej literatúry z roku 45 pred n.l., takže má viac ako 2000 rokov. Richard McClintock, profesor latinčiny na Hampden-Sydney College vo Virgínii, hľadal jedno z menej určitých latinských slov, consectetur, z pasáže Lorem Ipsum, a ako vyhľadával výskyt tohto slova v klasickej literatúre, objavil jeho nepochybný zdroj. Lorem Ipsum pochádza z odsekov 1.10.32 a 1.10.33 Cicerovho diela "De finibus bonorum et malorum" (O najvyššom dobre a zle), napísaného v roku 45 pred n.l. Táto kniha je pojednaním o teórii etiky, a bola veľmi populárna v renesancii. Prvý riadok Lorem Ipsum, "Lorem ipsum dolor sit amet..", je z riadku v odseku 1.10.32.</p>'
-            },
-            {
-                'title': 'Kde ho môžem získať?',
-                'description': '<p>Existuje mnoho podôb pasáží Lorem Ipsum, ale väčšina trpela rôznymi zmenami, vložením humoru, alebo náhodných slov, ktoré nevyzerajú ani trocha dôveryhodne.<br><br><br><br><br><br> Ak sa chystáte použiť pasáž z Lorem Ipsum, mali by ste sa presvedčiť, že uprostred textu nie je skrytá žiadna časť, ktorá by vás mohla priviesť do nepríjemnej situácie. Všetky generátory Lorem Ipsum na internete opakujú vopred definované časti textu, takže náš generátor je prvým skutočným generátorom na internete. Používa slovník viac ako 200 latinských slov, a kombinuje ich niekoľkými modelovými vetnými štruktúrami, takže generuje Lorem Ipsum, ktoré vyzerá hodnoverne. Vygenerované Lorem Ipsum je týmto spôsobom vždy bez opakujúcich sa častí, bez vtipov a nenáležitých výrazov, atď.</p>'
             }
         ];
     }
--- src/kcbb/slider/slider.component.ts
+++ src/kcbb/slider/slider.component.ts
@@ -1,9 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 
 @Component({
     selector: 'slider',
     templateUrl: './slider.html',
-    styleUrls: ['./slider.css']
+    styleUrls: ['./slider.css'],
+    animations: [
+        trigger('animateSlide', [
+            /* STATES */
+            state('slide-down, slide-left, slide-right', style({zIndex: 50})),
+            state('inactive', style({zIndex: 0})),
+            /* STYLES */
+            transition('* => slide-down', [
+                animate(400, keyframes([
+                    style({zIndex: 100, opacity: 0.5, transform: 'translateY(-100%)'}),
+                    style({zIndex: 100, opacity: 0.7, transform: 'translateY(-20%)'}),
+                    style({zIndex: 100, opacity: 1, transform: 'translateY(0)'})
+                ]))
+            ]),
+            transition('* => slide-left', [
+                animate(400, keyframes([
+                    style({zIndex: 100, opacity: 0.5, transform: 'translateX(100%)'}),
+                    style({zIndex: 100, opacity: 0.7, transform: 'translateX(20%)'}),
+                    style({zIndex: 100, opacity: 1, transform: 'translateX(0)'})
+                ]))
+            ]),
+            transition('* => slide-right', [
+                animate(400, keyframes([
+                    style({zIndex: 100, opacity: 0.5, transform: 'translateX(-100%)'}),
+                    style({zIndex: 100, opacity: 0.7, transform: 'translateX(-20%)'}),
+                    style({zIndex: 100, opacity: 1, transform: 'translateX(0)'})
+                ]))
+            ]),
+            transition('inactive <=> *', [
+                style({zIndex: 100}), animate(400)
+            ])
+        ])
+    ]
 })
 
 export class SliderComponent {
@@ -12,10 +44,12 @@ export class SliderComponent {
 
     public slides: any;
     public active: number;
+    public animation: any;
 
     constructor() {
         this.slides = [];
         this.active = 0;
+        this.animation = 'slide-down';
     }
 
     ngOnInit() {
@@ -25,14 +59,17 @@ export class SliderComponent {
     }
 
     public previousSlide(): void {
+        this.animation = 'slide-left';
         this.active = this.active > 0 ? this.active - 1 : this.slides.length - 1;
     }
 
     public nextSlide(): void {
+        this.animation = 'slide-right';
         this.active = this.active < this.slides.length - 1 ? this.active + 1 : 0;
     }
 
     public selectSlide(index: number): void {
+        this.animation = 'slide-down';
         this.active = index;
     }
 
