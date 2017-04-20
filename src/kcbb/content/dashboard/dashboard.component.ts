1c2af06e98be41d847dd1a6a642e52ba684ccc0c
Martin-Peter Lakatos <m16peter@yahoo.ro>
2017-04-20 18:55:57 +0200

    [*] dashboard refactoring...

--- src/kcbb/content/dashboard/dashboard.component.ts
+++ src/kcbb/content/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../../services/http/http.service';
 
 @Component({
     selector: 'app-dashboard',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
     styleUrls: ['./dashboard.less']
 })
 
-export class DashboardComponent { }
+export class DashboardComponent {
+
+    public loading: boolean = true;
+    public dashboard: any = [];
+
+    constructor(private httpService: HttpService) {
+        this.get();
+    }
+
+    private get(): void {
+
+        this.httpService
+            .get( '/dashboard' )
+            .subscribe(data => {
+                    console.log( data );
+                    this.dashboard = data;
+                    this.loading = false;
+                }, error => {}
+            );
+
+    }
+}
