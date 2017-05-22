0a8f6e7cdad1e24ea101ae54ad2445a542464368
Martin-Peter Lakatos <m16peter@yahoo.ro>
2017-05-22 11:16:24 +0200

    [-] build

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -26,11 +26,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes, { useHash: true })
-    ],
+    imports: [ RouterModule.forRoot(routes, { useHash: true }) ],
     exports: [ RouterModule ]
 })
+
 export class AppRoutingModule { }
 
 export const routedComponents = [
